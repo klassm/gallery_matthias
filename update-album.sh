bf01efc96742983ceff2eb5a7bd12f05a553e297
Matthias Klass <klassm@fhempy.fritz.box>
2019-05-25 16:38:35 +0200

    set binary location

--- update-album.sh
+++ update-album.sh
@@ -3,7 +3,7 @@ git reset --hard
 git fetch && git rebase 
 
 npm install
-node update_album.js
+PUPPETEER_CHROME_BIN="/usr/bin/chromium-browser" node update_album.js
 
 git add config.json
 git commit -am "update photos"
