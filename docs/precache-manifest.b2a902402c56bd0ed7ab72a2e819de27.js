5c59fc3405660aa8630d14c84be9aa8b8a25c27b
Matthias Klass <matthias.klass@gmail.com>
2019-05-30 09:25:37 +0200

    Keep aspect ratio

---
+++ docs/precache-manifest.b2a902402c56bd0ed7ab72a2e819de27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ec5f579b025cd7dc93b26d1dc81c06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd9c4cc433582027c8ca",
+    "url": "/static/css/2.1c29fa96.chunk.css"
+  },
+  {
+    "revision": "6c76036d263e06a0f390",
+    "url": "/static/css/main.a25de3d2.chunk.css"
+  },
+  {
+    "revision": "dd9c4cc433582027c8ca",
+    "url": "/static/js/2.805061d1.chunk.js"
+  },
+  {
+    "revision": "6c76036d263e06a0f390",
+    "url": "/static/js/main.0f389a5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
