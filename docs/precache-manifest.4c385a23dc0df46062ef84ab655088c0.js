a87d14c3dc52043a15b71e63e4e8732c90ceed6d
Matthias Klass <matthias.klass@gmail.com>
2019-05-30 09:09:50 +0200

    Update prod build

---
+++ docs/precache-manifest.4c385a23dc0df46062ef84ab655088c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da3e4ba82687773feba4d09f75cb253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd9c4cc433582027c8ca",
+    "url": "/static/css/2.1c29fa96.chunk.css"
+  },
+  {
+    "revision": "f8b2c7343c22928d158c",
+    "url": "/static/css/main.4bf66956.chunk.css"
+  },
+  {
+    "revision": "dd9c4cc433582027c8ca",
+    "url": "/static/js/2.805061d1.chunk.js"
+  },
+  {
+    "revision": "f8b2c7343c22928d158c",
+    "url": "/static/js/main.70274c1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
