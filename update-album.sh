515556589eecd6b01b11eb0c006aae84e70a9ca5
Matthias Klass <matthias.klass@gmail.com>
2019-05-30 09:38:05 +0200

    update update-album.sh

--- update-album.sh
+++ update-album.sh
@@ -5,7 +5,7 @@ git fetch && git rebase
 npm install
 PUPPETEER_CHROME_BIN="/usr/bin/chromium-browser" node update-album.js
 
-git add web/public/config.json
+git add site/public/config.json
 git add docs/config.json
 git commit -am "update photos"
 git push
