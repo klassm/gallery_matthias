6456b07d17103f0bb7f0e4c17e2b43f2605a882e
Matthias Klass <matthias.klass@gmail.com>
2019-05-30 09:00:23 +0200

    Add prod

---
+++ docs/precache-manifest.7e69afe7df7273a314dd7893447d8e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3796a183fe50f3e4f364200f4844623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd9c4cc433582027c8ca",
+    "url": "/static/css/2.1c29fa96.chunk.css"
+  },
+  {
+    "revision": "4bf5882213353633c760",
+    "url": "/static/css/main.4bf66956.chunk.css"
+  },
+  {
+    "revision": "dd9c4cc433582027c8ca",
+    "url": "/static/js/2.805061d1.chunk.js"
+  },
+  {
+    "revision": "4bf5882213353633c760",
+    "url": "/static/js/main.f73dd718.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
