0331d48d9a557766e4a99b675c8bb10d09f9a671
Matthias Klass <matthias.klass@gmail.com>
2019-06-22 14:01:43 +0200

    Add missing files ...

---
+++ docs/precache-manifest.03b84408e037c85f31c16cc7f8dd7a47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40f7a72b74ba98f5b62a9fb9995f1f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd9c4cc433582027c8ca",
+    "url": "/static/css/2.1c29fa96.chunk.css"
+  },
+  {
+    "revision": "84eb0e23a1bcf9445b95",
+    "url": "/static/css/main.a9816d43.chunk.css"
+  },
+  {
+    "revision": "dd9c4cc433582027c8ca",
+    "url": "/static/js/2.805061d1.chunk.js"
+  },
+  {
+    "revision": "84eb0e23a1bcf9445b95",
+    "url": "/static/js/main.a541937b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
